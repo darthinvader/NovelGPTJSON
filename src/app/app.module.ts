deafcea04524ad5d02261d4f500ab2c61fc2847d
Ioannis <psaradakisj@gmail.com>
2023-08-02 21:56:03 +0200

    Refactor: Added jsons, removed old components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { CharacterComponent } from './character/character.component';
 import { CardComponent } from './card/card.component';
-import { TextInputComponent } from './text-input/text-input.component';
-import { TextArrayInputComponent } from './text-array-input/text-array-input.component';
 import { FormsComponent } from './forms/forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterComponent,
     CardComponent,
-    TextInputComponent,
-    TextArrayInputComponent,
     FormsComponent,
   ],
   imports: [
